fce892dc95447c687acecde4390679f1801e570c
Sonali553 <83964942+Sonali553@users.noreply.github.com>
2021-05-16 13:38:12 +0530

    Sonali.py

---
+++ Sonali.py
@@ -0,0 +1,13 @@
+# finding the area
+import math
+r=input("radius of the circle=")
+Area=math.pi*r*r
+Print(Area)
+
+#print extension of file
+
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
